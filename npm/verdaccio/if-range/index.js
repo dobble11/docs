6d50fcf0bc78e8597107eaf396b70168715aec04
zhangjiawei <zhang.jiawei@intellif.com>
2018-11-15 10:27:10 +0800

    2121

--- npm/verdaccio/if-range/index.js
+++ npm/verdaccio/if-range/index.js
@@ -3,9 +3,9 @@ module.exports = function() {
     start = 0,
     end = 0;
 
-  if (arguments.length == 1) {
+  if (arguments.length === 1) {
     end = arguments[0];
-  } else if (arguments.length == 2) {
+  } else if (arguments.length === 2) {
     [start, end] = arguments;
   }
 
